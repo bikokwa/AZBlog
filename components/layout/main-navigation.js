43d84c25c56dd831a7e6197843bd2b301d94db73
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-28 18:47:52 +0300

    add layout and navigation

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+function ManNavigation() {
+  return (
+    <header>
+      <Link href="/">
+        <Logo />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default ManNavigation;
