642a4aceeb051c1774537df8edae6202bec7c63e
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-28 17:42:27 +0300

    add hero component

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,6 +1,24 @@
 import Image from "next/image";
 import classes from "./hero.module.css";
 
-function Hero() {}
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image showing Max"
+          height={300}
+          width={300}
+        />
+      </div>
+      <h1>Hi, I'm Max</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+}
 
 export default Hero;
