5a7c741e86643d1415e13898c79edc74a414a1a2
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-29 18:30:57 +0300

    add All Posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/home-page/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "nextjs-file-based-routing.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
+    date: "2022-02-10",
+  },
+  {
+    slug: "mastering-javascript",
+    title: "Mastering JavaScript",
+    image: "mastering-js-thumb.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
+    date: "2022-02-10",
+  },
+];
+
+function AllPostsPage() {
+  return (
+    <>
+      <AllPosts posts={DUMMY_POSTS} />
+    </>
+  );
+}
 
 export default AllPostsPage;
