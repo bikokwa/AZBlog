2ecaecb69787075db4a6e22ef047abf79e070cc4
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-30 20:38:33 +0300

    render images

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,24 @@
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "nextjs-file-based-routing.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
-    date: "2022-02-10",
-  },
-  {
-    slug: "mastering-javascript",
-    title: "Mastering JavaScript",
-    image: "mastering-js-thumb.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
-    date: "2022-02-10",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,38 +1,22 @@
 import AllPosts from "../../components/home-page/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "nextjs-file-based-routing.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
-    date: "2022-02-10",
-  },
-  {
-    slug: "mastering-javascript",
-    title: "Mastering JavaScript",
-    image: "mastering-js-thumb.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps a breeze!",
-    date: "2022-02-10",
-  },
-];
-
-function AllPostsPage() {
+function AllPostsPage(props) {
   return (
     <>
-      <AllPosts posts={DUMMY_POSTS} />
+      <AllPosts posts={props.posts} />
     </>
   );
+}
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 }
 
 export default AllPostsPage;
